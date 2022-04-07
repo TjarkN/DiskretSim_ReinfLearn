a6b0b00d9239e899e546a449ec2b633823c8e43a
TjarkN <github@tja-rk.de>
2022-04-07 16:30:00 +0200

    Committed files

--- VC_World_Reflex_Agent/agents/simple_reflex_agent.py
+++ VC_World_Reflex_Agent/agents/simple_reflex_agent.py
@@ -23,9 +23,9 @@ class SimpleReflexAgent(Agent):
 
     def create_q_table(self):
         q_table = np.zeros((len(self.states), (len(self.actions))))
-        q_table[0, 1] = 58
 
         # Put your source code here
+
         # w.g. q_table[0, 1] = 5 asserts a q_value of 5 to perform action 1 in state 0
         # the corresponding states and actions can be obtained by self.states[0] and
         # self.actions[1] in this example
