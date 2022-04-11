6cec54b8bc5c1d01061c1d986f068cac3a575493
TjarkN <github@tja-rk.de>
2022-04-11 13:52:50 +0200

    Added gitignore

--- VC_World_Reflex_Agent/agents/simple_reflex_agent.py
+++ VC_World_Reflex_Agent/agents/simple_reflex_agent.py
@@ -23,7 +23,6 @@ class SimpleReflexAgent(Agent):
 
     def create_q_table(self):
         q_table = np.zeros((len(self.states), (len(self.actions))))
-
         # Put your source code here
         # Nothing to see here
         # w.g. q_table[0, 1] = 5 asserts a q_value of 5 to perform action 1 in state 0
